1b930989afe4bde91b330c03b2380e244d8e0be8
Lst11 <stanishevskaya.lena@gmail.com>
2018-09-19 14:37:45 +0300

    Lesson_2

---
+++ app/src/main/java/by/it_academy/ad02_09_2018_class/Lesson2Activity.java
@@ -0,0 +1,17 @@
+package by.it_academy.ad02_09_2018_class;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.text.Html;
+import android.widget.TextView;
+
+public class Lesson2Activity extends Activity {
+
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.lesson2);
+//        TextView textView = findViewById("forgotPassword");
+//        textView.setText(Html.fromHtml("<u>" + "@string/forgotPassword" + "</u>"));
+    }
+
+}
